cb7462b3dce23c5a32317a63f0cf23959f217c79
Davide Murro <118051417+davide-murro@users.noreply.github.com>
2025-09-25 23:03:37 +0200

    build test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://localhost:7203/api'
-  apiUrl: 'https://87.8.7.81:7203/api'
+  apiUrl: 'https://localhost:7203/api'
+  //apiUrl: 'http://localhost:5000/api'
+  //apiUrl: 'https://87.8.7.81:5000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://localhost:7203/api'
-  apiUrl: 'https://87.8.7.81:7203/api'
+  apiUrl: 'https://87.8.7.81:5000/api'
 };
