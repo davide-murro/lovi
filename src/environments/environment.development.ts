0c373c85d4a40ad6351e72d7327bae802c8f0e38
Davide Murro <118051417+davide-murro@users.noreply.github.com>
2025-09-27 18:45:24 +0200

    test build

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:7203/api'
   //apiUrl: 'http://localhost:5000/api'
-  //apiUrl: 'https://87.8.7.81:5000/api'
+  //apiUrl: 'http://79.16.234.244:5000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://87.8.7.81:5000/api'
+  apiUrl: 'http://79.16.234.244:5000/api'
 };
